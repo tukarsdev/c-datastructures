b34691f5a0f2f85cee8b434a0d112934e7cdb0cc
masterofcapacitors <183424959+masterofcapacitors@users.noreply.github.com>
2025-06-03 19:05:44 +0100

    Update c_standard.h to have both c versions and c++ versions

--- src/c_standard.h
+++ src/c_standard.h
@@ -5,22 +5,53 @@
 extern "C" {
 #endif
 
-
 #define PREDEF_STANDARD_C89
+
+#ifdef __STDC_VERSION__
 
 #if __STDC_VERSION__ >= 199901L
     #define PREDEF_STANDARD_C99
 #endif
-
 #if __STDC_VERSION__ >= 201112L
     #define PREDEF_STANDARD_C11
 #endif
+#if __STDC_VERSION__ >= 201710L
+    #define PREDEF_STANDARD_C17
+#endif
+#if __STDC_VERSION__ >= 202311L
+    #define PREDEF_STANDARD_C23
+#endif
 
+#endif // __STDC_VERSION__
+
+#ifdef __cplusplus
+
+#define PREDEF_STANDARD_CXX98
+#if __cplusplus >= 201103L
+    #define PREDEF_STANDARD_CXX11
+#endif
+#if __cplusplus >= 201402L
+    #define PREDEF_STANDARD_CXX14
+#endif
+#if __cplusplus >= 201703L
+    #define PREDEF_STANDARD_CXX17
+#endif
+#if __cplusplus >= 202002L
+    #define PREDEF_STANDARD_CXX20
+#endif
+#if __cplusplus >= 202302L
+    #define PREDEF_STANDARD_CXX23
+#endif
+
+#endif // __cplusplus
+
+#if defined(PREDEF_STANDARD_C99) || defined(PREDEF_STANDARD_CXX11)
+    #define PREDEF_STANDARD_C99_CXX11
+#endif
 
 #ifdef __cplusplus
 }
 #endif
 
 #endif // ifndef C_STANDARD_H
-
 
